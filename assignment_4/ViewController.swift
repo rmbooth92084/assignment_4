8a6b6f52b0ce7bd653684af6412a82fcf0d23499
Raymond Booth <rmbooth92084@gmail.com>
2022-11-01 11:00:27 -0700

    Implemented all the buttons and works as regular calculator but it cannot handle inputting 2 operations in a row without a number in between or pressing equals buttons to do the last operation again to the current result

--- assignment_4/ViewController.swift
+++ assignment_4/ViewController.swift
@@ -9,11 +9,200 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var calOutput: UILabel!
+    var currOuput : Double = 0
+    var valTemp : Double = 0
+    var currOp = "+"
+    var nextInput : Bool = true
+    var decimal : Bool = false
+    var shift : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calOutput.text = String(currOuput)
     }
-
-
+    func reset(){
+        valTemp = 0
+        currOuput = 0
+        shift = 0
+        decimal = false
+    }
+    /*
+     This funciton handles the real time output of the calculator so you can see the
+     numbers you are inputting into the calculator
+     */
+    func updateOutput(input : Double){
+        if(currOuput == 0){
+            currOuput = input
+            calOutput.text = String(currOuput)
+        }
+        else{
+            if(decimal){
+                shift += 1
+            }
+            
+            currOuput *= 10
+            currOuput += input
+            if(decimal){
+                calOutput.text = String(currOuput / pow(10.0, Double(shift)))
+            }
+            else{
+                calOutput.text = String(currOuput)
+                
+            }
+            
+        }
+        
+    }
+    /*
+     This funciton handles a what each operation button does
+     */
+    var temp : Double = 0.0
+    var equlAgain : Bool = false
+    func calulate(input : String){
+       
+        if(nextInput){
+            if(decimal){
+                currOuput /= pow(10.0, Double(shift))
+            }
+            decimal = false
+            shift = 0
+            switch(input){
+                case "+" :
+                    valTemp += currOuput
+                case "-" :
+                if(valTemp == 0){
+                    valTemp = currOuput
+                }
+                else{
+                    valTemp -= currOuput
+                }
+                    
+                case "x" :
+                    if(valTemp == 0){
+                        valTemp = currOuput
+                    }
+                    else{
+                        valTemp *= currOuput
+                    }
+                case "/" :
+                    if(valTemp == 0){
+                        valTemp = currOuput
+                    }
+                    else{
+                        valTemp /= currOuput
+                    }
+                case "%" :
+                    if(valTemp == 0){
+                        valTemp = currOuput
+                    }
+                    else{
+                        valTemp = Double(Int(valTemp) % Int(currOuput))
+                    }
+                case "=" :
+                    temp = currOuput
+                    equlAgain = true
+                    calulate(input: currOp)
+                default :
+                    print("Invalid Input")
+                
+                
+            }
+            if(equlAgain == false){
+                currOuput = 0
+            }
+            else{
+                currOuput = temp
+                equlAgain = false
+            }
+            if(input != "="){
+                currOp = input
+            }
+        }
+        
+    }
+    
+    //operations
+    
+    @IBAction func equButton(_ sender: Any) {
+        print("= pressed")
+        print(temp)
+        calulate(input: "=")
+        calOutput.text = String(valTemp)
+        
+    }
+    @IBAction func plusButton(_ sender: Any) {
+        calulate(input: "+")
+        
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        calulate(input: "-")
+        
+    }
+    @IBAction func divButton(_ sender: Any) {
+        calulate(input: "/")
+        
+    }
+    @IBAction func modButton(_ sender: Any) {
+        calulate(input: "%")
+        
+    }
+    @IBAction func mutlButton(_ sender: Any) {
+        calulate(input: "x")
+        
+    }
+    
+    @IBAction func decButton(_ sender: Any) {
+        decimal = true
+    }
+    
+    
+    //misc.
+    @IBAction func clearButton(_ sender: Any) {
+        reset()
+        calOutput.text = String(currOuput)
+        
+    }
+    
+    @IBAction func signButton(_ sender: Any) {
+        currOuput *= -1
+        calOutput.text = String(currOuput)
+        
+    }
+    
+    //number buttons
+    @IBAction func zeroButton(_ sender: Any) {
+        updateOutput(input: 0)    }
+    @IBAction func oneButton(_ sender: Any) {
+        updateOutput(input: 1)    }
+    
+    @IBAction func twoButton(_ sender: Any) {
+        updateOutput(input: 2)    }
+    
+    @IBAction func threeButton(_ sender: Any) {
+        updateOutput(input: 3)    }
+    
+    @IBAction func fourButton(_ sender: Any) {
+        updateOutput(input: 4)    }
+    
+    @IBAction func fiveButton(_ sender: Any) {
+        updateOutput(input: 5)    }
+    
+    @IBAction func sixButton(_ sender: Any) {
+        updateOutput(input: 6)    }
+    
+    @IBAction func sevenButton(_ sender: Any) {
+        updateOutput(input: 7)    }
+    
+    @IBAction func eightButton(_ sender: Any) {
+        updateOutput(input: 8)    }
+    
+    @IBAction func nineButton(_ sender: Any) {
+        updateOutput(input: 9)    }
+    
+    
+    
 }
 
